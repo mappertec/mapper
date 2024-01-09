04116c855ce566fde376d4adf2df1d2cae79b225
Dan Humphreys <dan.humphreys@xde.dev>
2024-01-09 23:22:55 +0000

    Changed version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.21"
     `maven-publish`
+    id("com.github.johnrengelman.shadow") version("8.1.1")
 }
 
 group = "dev.mapper"
@@ -21,3 +22,21 @@ repositories {
 //kotlin {
 //    jvmToolchain(8)
 //}
+
+tasks {
+
+    shadowJar {
+        val folder = System.getenv("pluginFolder_1-20")
+        if (folder != null) destinationDirectory.set(file(folder))
+        archiveFileName.set("${project.name}-${project.version}.jar")
+    }
+
+    processResources{
+        expand(project.properties)
+
+        inputs.property("version", rootProject.version)
+        filesMatching("plugin.yml") {
+            expand("version" to rootProject.version)
+        }
+    }
+}
