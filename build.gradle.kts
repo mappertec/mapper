2df42251614fc24a2f742f84a7775afc16a9672f
Dan Humphreys <dan.humphreys@xde.dev>
2024-01-09 23:29:44 +0000

    Changed version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,43 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+    java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version("8.1.1")
 }
 
-group = "dev.mapper"
-version = "v0.5.0"
+group = "me.dave"
+version = "2.0-BETA"
 
 repositories {
     mavenCentral()
+    mavenLocal() // PlatyUtils
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") } // Spigot
+    maven { url = uri("https://repo.dmulloy2.net/repository/public/")} // ProtocolLib
+    maven { url = uri("https://maven.playpro.com")} // CoreProtect
+    maven { url = uri("https://repo.william278.net/snapshots") } // HuskClaims
+    maven { url = uri("https://repo.william278.net/releases") } // HuskTowns
+    maven { url = uri("https://jitpack.io")} // ChatColorHandler, GardeningTweaks, GriefPrevention, RealisticBiomes
 }
 
-//dependencies {
-//    //implementation("com.google.code.gson:gson:2.10.1")
-//    testImplementation("org.jetbrains.kotlin:kotlin-test")
-//}
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("com.comphenix.protocol:ProtocolLib:4.7.0")
+    compileOnly("net.coreprotect:coreprotect:21.3")
+    compileOnly("com.github.Maroon28:RealisticBiomes:3d292ea32a")
+    compileOnly("net.william278.huskclaims:huskclaims-bukkit:1.0-8818300")
+    compileOnly("net.william278:husktowns:2.6.1")
+    compileOnly("com.github.TechFortress:GriefPrevention:16.18")
+    implementation(files("libs/PlatyUtils-0.1.18.jar"))
+    implementation("com.github.CoolDCB:ChatColorHandler:v2.1.5")
+}
 
-//tasks.test {
-//    useJUnitPlatform()
-//}
-//kotlin {
-//    jvmToolchain(8)
-//}
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
 
 tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
 
     shadowJar {
         val folder = System.getenv("pluginFolder_1-20")
